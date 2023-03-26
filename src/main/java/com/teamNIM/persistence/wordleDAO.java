b320505b1d6487a1cc3e33a02289217e83fb9e1a
Keith <klienert@madisoncollege.edu>
2023-03-25 21:12:24 -0500

    wordleDAO added

--- src/main/java/com/teamNIM/persistence/wordleDAO.java
+++ src/main/java/com/teamNIM/persistence/wordleDAO.java
@@ -1,12 +1,19 @@
 package com.teamNIM.persistence;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamNIM.entity.WordleAnswers;
 import com.teamNIM.util.PropertiesLoader;
 
 import java.io.IOException;
 import java.util.Properties;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.*;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.MediaType;
 
+/**
+ * The type Wordle dao.
+ */
 public class wordleDAO implements PropertiesLoader {
     Properties properties;
     private final Logger logger = LogManager.getLogger(this.getClass());
@@ -25,4 +32,30 @@ public class wordleDAO implements PropertiesLoader {
             logger.error("Error loading properties..." + e);
         }
     }
+
+    public WordleAnswers getAnswer(String date) {
+        Client client = ClientBuilder.newClient();
+        WebTarget target =
+                client.target(url_key() + date + ".json");
+        String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        WordleAnswers newWord = new WordleAnswers();
+        try {
+            newWord = mapper.readValue(response, WordleAnswers.class);
+        } catch (JsonProcessingException e) {
+            logger.error("Error processing JSON... " + e);
+        }
+        return newWord;
+    }
+
+    /**
+     * Helper method, load properties and get correct URL
+     * **May need to change this to a different API
+     * @return  String with API URI prefix
+     */
+    private String url_key () {
+        loadProperties();
+        return properties.getProperty("wordle.nytimes");
+    }
+
 }
