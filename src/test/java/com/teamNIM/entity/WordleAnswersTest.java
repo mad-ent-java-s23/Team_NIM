e9cdb046b292336b99fd84ef2cc451a714b520ee
Keith <klienert@madisoncollege.edu>
2023-03-25 21:11:48 -0500

    added unit tests

--- src/test/java/com/teamNIM/entity/WordleAnswersTest.java
+++ src/test/java/com/teamNIM/entity/WordleAnswersTest.java
@@ -1,4 +1,39 @@
+package com.teamNIM.entity;
+
+import com.teamNIM.persistence.wordleDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for wordleDAO
+ * @author keithlienert
+ */
 class WordleAnswersTest {
-  
+    wordleDAO dao;
+    LocalDate date;
+
+    @BeforeEach
+    void setUp() {
+        date = LocalDate.now(); // current date
+        dao = new wordleDAO();
+    }
+
+    @Test
+    public void getCurrentSolutionTestSuccess() {
+        assertEquals("voter", dao.getAnswer(String.valueOf(date)).getSolution());
+    }
+
+    @Test
+    public void getOlderSolutionTestSuccess() {
+        assertEquals("moose", dao.getAnswer("2023-03-01").getSolution());
+        assertEquals("above", dao.getAnswer("2023-03-02").getSolution());
+        assertEquals("squat", dao.getAnswer("2023-03-03").getSolution());
+        assertEquals("trend", dao.getAnswer("2023-03-04").getSolution());
+        assertEquals("toxic", dao.getAnswer("2023-03-05").getSolution());
+    }
+
 }
