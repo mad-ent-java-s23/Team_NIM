8a7a1fa2b19cf8d3ed1fc5b0f7c8b7e760ca289f
Keith <klienert@madisoncollege.edu>
2023-04-02 15:30:20 -0500

    updated the service package with service classes -- link on index page should work

--- src/main/java/com/teamNIM/service/WordleApplication.java
+++ src/main/java/com/teamNIM/service/WordleApplication.java
@@ -1,2 +1,22 @@
-package com.teamNIM.service;public class WordleApplication {
+package com.teamNIM.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Defines the base URI for all resource URIs (where the APIs are located
+ */
+@ApplicationPath("/getWords")
+
+/**
+ * Declares the root resource and provider classes
+ */
+public class WordleApplication extends Application {
+    public Set<Class<?>> getWords() {
+        HashSet h = new HashSet<Class<?>>();
+        h.add(WordleService.class);
+        return h;
+    }
 }
